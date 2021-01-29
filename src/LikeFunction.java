249d879ccecc6e192fe7a0343d218111bb2382c4
Thwe Oo <mathweookhine@gmail.com>
2021-01-29 16:51:19 +0630

    spaces are added

--- src/LikeFunction.java
+++ src/LikeFunction.java
@@ -2,6 +2,8 @@
 public class LikeFunction {
 
 	public static void main(String[] args) {
+
+
 		System.out.println(Solution.whoLikesIt());
 		System.out.println(Solution.whoLikesIt("Peter "));
 		System.out.println(Solution.whoLikesIt("Jacob", "Alex"));
